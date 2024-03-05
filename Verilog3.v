8ee016949b86fb1241d988fdadab37428b0e8b21
Chad Burrow <stevenchadburrow@gmail.com>
2024-03-05 05:27:38 -0600

    Add files via upload

---
+++ Verilog3.v
@@ -0,0 +1,312 @@
+
+
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:41:04 11/28/2023 
+// Design Name: 
+// Module Name:    Verilog4 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+
+Make sure to run fitter like in command line, it allows you to change -optimize to "density" which makes things fit!
+
+cd ~/Xilinx/JustPIC ; ~/Xilinx/14.7/ISE_DS/ISE/bin/lin64/cpldfit -intstyle ise -p xc9572xl-5-VQ44 -ofmt vhdl -optimize density -htmlrpt -loc on -slew fast -init low -inputs 54 -pterms 25 -unused float -power std -terminate keeper Verilog3.ngd ; ~/Xilinx/14.7/ISE_DS/ISE/bin/lin64/hprep6 -s IEEE1149 -n Verilog3 -i Verilog3
+
+cd /usr/share ; sudo xc3sprog -c xpc ~/Xilinx/JustPIC/Verilog3.jed
+
+Use 'xc3sprog' to program the .jed file into the CPLD, but you need to follow some instructions:
+
+Notes for the USB cable
+=======================
+
+To use the device as an ordinary user, put the following line in a new
+file "libusb-driver.rules" in /etc/udev/rules.d/ and restart udev:
+ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="03fd", MODE="666"
+
+
+If your cable does not have the ID 03fd:0008 in the output of lsusb,
+the initial firmware has not been loaded (loading it changes the
+product-ID from another value to 8). To load the firmware follow
+these steps:
+
+1. Run ./setup_pcusb in this directory, this should set up everything
+   correctly:
+   - When $XILINX is set correctly:
+     $ ./setup_pcusb
+   - When $XILINX is not set, and ISE is installed in /opt/Xilinx/13.1:
+     $ ./setup_pcusb /opt/Xilinx/13.1/ISE_DS/ISE
+
+Old instructions, use only when the above script did not work:
+
+1. If you have no /etc/udev/rules.d/xusbdfwu.rules file, copy it from
+   /path/to/ISE/bin/lin/xusbdfwu.rules to /etc/udev/rules.d/xusbdfwu.rules
+
+2. If you are running a newer version of udev (as in Debian Squeeze and
+   Ubuntu 9.10), you need to adapt the rules-file to the new udev-version:
+   sed -i -e 's/TEMPNODE/tempnode/' -e 's/SYSFS/ATTRS/g' -e 's/BUS/SUBSYSTEMS/' /etc/udev/rules.d/xusbdfwu.rules
+
+3. Install the package containing /sbin/fxload from your linux distribution.
+   It is usually called "fxload"
+
+4. copy the files /path/to/ISE/bin/lin/xusb*.hex to /usr/share/
+
+5. restart udev and re-plug the cable with 'service udev restart'
+
+That should do it???
+
+*/
+
+// This is for the PIC-only system.  
+// It should run at 6.28 MHz, giving 16 colors at 320x240 and using 32KB of RAM.
+// The PIC will interface using it's Parallel Master Port (technically Parallel Slave Port here)
+// much like a 6502 would.  The PIC takes care of it's own addresses.
+// This CPLD should have enough room to do vertical OR horizontal scrolling if pre-programmed to do so.
+
+
+module Verilog3(
+	input master_clock, // 25.175 MHz
+	output video, // 6.28 MHz
+	output not_video, // inverted clock
+	input write, // signals from PIC, want to write when low
+	input addr, // signals from PIC, unused?
+	output reg hsync, // video sync signals
+	output reg vsync,
+	output ram_ce, // /CE for RAM
+	output ram_oe, // /OE for RAM
+	output ram_we, // /WE for RAM
+	output pic_prd, // parallel read (high enabled)
+	output pic_pwr, // parallel write (high enabled)
+	output pic_pcs, // parallel chip select (high enabled)
+	inout [7:0] address,
+	inout [7:0] data,
+	output latch, // latches upper address (during hsync)
+	output reg red,
+	output reg green,
+	output reg blue,
+	output reg intensity
+    );
+
+reg half;
+reg quarter;
+reg eighth;
+reg [17:0] video_addr; // video addresses
+reg [5:0] color_data;
+reg hblank; // blanking values
+reg vblank;
+reg vscreen;
+reg prev;
+reg ready;
+
+assign video = eighth;
+assign not_video = ~eighth;
+
+assign latch = ~hsync;
+
+// these are A0-A7, changed each pixel, inactive during phi2 high or booting,
+// these supply A8-A15 during h-sync for external latch
+assign address[6:0] = ((~latch && ~eighth) ? video_addr[16:10] :
+	((~eighth) ? video_addr[6:0] :
+	7'bzzzzzzz));
+assign address[7] = ((~latch && ~eighth) ? video_addr[17] :
+	((~eighth) ? prev :
+	1'bz));
+	
+assign ram_ce = 1'b0;
+assign ram_oe = (~eighth ? 1'b0 : 1'b1);
+assign ram_we = (~eighth ? 1'b1 : ((~ready && eighth && quarter) ? 1'b0 : 1'b1));
+
+assign pic_pcs = (~eighth ? 1'b1 : 1'b0);
+assign pic_prd = (~eighth ? 1'b1 : (~ready ? 1'b0 : 1'b1));
+assign pic_pwr = (~eighth ? 1'b1 : 1'b1);
+
+assign data[7:0] = 8'bzzzzzzzz;
+
+
+
+always @(negedge master_clock) begin
+	
+	half <= ~half;
+	
+	if (half) begin
+		quarter <= ~quarter;
+		
+		if (quarter) begin
+			eighth <= ~eighth;	
+		end
+	end
+	
+	if (half && ~quarter && eighth) begin
+		if (hblank && vscreen && ~vblank) begin
+			red <= 1'b0;
+			green <= 1'b0;
+			blue <= 1'b0;
+			intensity <= 1'b1;
+		end
+		else if (hblank && vscreen && vblank) begin
+			red <= color_data[5];
+			green <= color_data[4];
+			blue <= color_data[4];
+			intensity <= 1'b0;
+		end
+		else begin
+			red <= 1'b0;
+			green <= 1'b0;
+			blue <= 1'b0;
+			intensity <= 1'b0;
+		end
+	end
+	else if (half && quarter && eighth) begin	
+		ready <= 1'b1;
+	
+		if (hblank && vscreen && ~vblank) begin
+			red <= 1'b0;
+			green <= 1'b0;
+			blue <= 1'b0;
+			intensity <= 1'b1;
+		end
+		else if (hblank && vscreen && vblank) begin
+			red <= color_data[3];
+			green <= color_data[2];
+			blue <= color_data[2];
+			intensity <= 1'b0;
+		end
+		else begin
+			red <= 1'b0;
+			green <= 1'b0;
+			blue <= 1'b0;
+			intensity <= 1'b0;
+		end
+	end
+	else if (half && ~quarter && ~eighth) begin		
+		if (hblank && vscreen && ~vblank) begin
+			red <= 1'b0;
+			green <= 1'b0;
+			blue <= 1'b0;
+			intensity <= 1'b1;
+		end
+		else if (hblank && vscreen && vblank) begin
+			red <= color_data[1];
+			green <= color_data[0];
+			blue <= color_data[0];
+			intensity <= 1'b0;
+		end
+		else begin
+			red <= 1'b0;
+			green <= 1'b0;
+			blue <= 1'b0;
+			intensity <= 1'b0;
+		end
+	end
+	else if (half && quarter && ~eighth) begin
+		color_data[5:0] <= data[5:0];
+		ready <= write;
+	
+		if (hblank && vscreen && ~vblank) begin
+			red <= 1'b0;
+			green <= 1'b0;
+			blue <= 1'b0;
+			intensity <= 1'b1;
+		end
+		else if (hblank && vscreen && vblank) begin
+			red <= data[7];
+			green <= data[6];
+			blue <= data[6];
+			intensity <= 1'b0;
+		end
+		else begin
+			red <= 1'b0;
+			green <= 1'b0;
+			blue <= 1'b0;
+			intensity <= 1'b0;
+		end
+	end
+	
+	if (half && quarter && ~eighth) begin
+	
+		// video sync signals
+		
+		if (video_addr[7:0] == 8'b01101000) begin
+			hsync <= 1'b1;
+		end
+		
+		if (video_addr[7:0] == 8'b01111000) begin
+			hsync <= 1'b0;
+			prev <= video_addr[9];
+		end
+
+		if (video_addr[7:0] == 8'b10000011) begin	
+			if (video_addr[17:8] == 10'b1001011001) begin
+				vsync <= 1'b1;
+			end
+			
+			if (video_addr[17:8] == 10'b1001011101) begin
+				vsync <= 1'b0;
+			end
+
+			if (video_addr[17:8] == 10'b1001110011) begin
+				video_addr[17:8] <= 10'b0000000000;
+				//vblank <= 1'b1;
+			end
+			else begin
+				video_addr[17:8] <= video_addr[17:8] + 1;
+			end
+			
+			video_addr[7:0] <= 8'b00000000;
+			//hblank <= 1'b1;
+		end
+		else begin
+			video_addr[7:0] <= video_addr[7:0] + 1;
+		end
+	end
+	
+	if (half && quarter && ~eighth) begin
+	
+		// video blanking signals
+	
+		if (video_addr[7:0] == 8'b00000000) begin
+			hblank <= 1'b1;
+		end
+		
+		if (video_addr[7:0] == 8'b01100100) begin
+			hblank <= 1'b0;
+		end
+		
+		if (video_addr[7:0] == 8'b10000011) begin	
+			if (video_addr[17:8] == 10'b0111111111) begin
+				vblank <= 1'b0;
+			end
+			
+			if (video_addr[17:8] == 10'b1001011000) begin
+				vscreen <= 1'b0;
+			end
+			
+			if (video_addr[17:8] == 10'b0000000000) begin
+				vblank <= 1'b1;
+				vscreen <= 1'b1;
+			end
+		end
+	end
+end
+
+	
+endmodule
+
+
+
+
---
+++ configuration_bits.c
@@ -0,0 +1,98 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+// PIC18F47J13 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1L
+#pragma config WDTEN = OFF      // Watchdog Timer (Disabled - Controlled by SWDTEN bit)
+#pragma config PLLDIV = 1       // 96MHz PLL Prescaler Selection (PLLSEL=0) (No prescale (4 MHz oscillator input drives PLL directly))
+#pragma config CFGPLLEN = ON    // PLL Enable Configuration Bit (PLL Enabled)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset (Enabled)
+#pragma config XINST = OFF      // Extended Instruction Set (Disabled)
+
+// CONFIG1H
+#pragma config CP0 = OFF        // Code Protect (Program memory is not code-protected)
+
+// CONFIG2L
+#pragma config OSC = ECPLL
+#pragma config SOSCSEL = DIG    // T1OSC/SOSC Power Selection Bits (Digital (SCLKI) mode selected)
+#pragma config CLKOEC = ON      // EC Clock Out Enable Bit  (CLKO output enabled on the RA6 pin)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor (Disabled)
+#pragma config IESO = OFF       // Internal External Oscillator Switch Over Mode (Disabled)
+
+// CONFIG2H
+#pragma config WDTPS = 32768    // Watchdog Postscaler (1:32768)
+
+// CONFIG3L
+#pragma config DSWDTOSC = INTOSCREF// DSWDT Clock Select (DSWDT uses INTRC)
+#pragma config RTCOSC = INTOSCREF// RTCC Clock Select (RTCC uses INTRC)
+#pragma config DSBOREN = OFF    // Deep Sleep BOR (Disabled)
+#pragma config DSWDTEN = OFF    // Deep Sleep Watchdog Timer (Disabled)
+#pragma config DSWDTPS = 2      // Deep Sleep Watchdog Postscaler (1:2 (2.1 ms))
+
+// CONFIG3H
+#pragma config IOL1WAY = OFF    // IOLOCK One-Way Set Enable bit (The IOLOCK bit (PPSCON<0>) can be set and cleared as needed)
+#pragma config ADCSEL = BIT10   // ADC 10 or 12 Bit Select (10 - Bit ADC Enabled)
+#pragma config PLLSEL = PLL4X   // PLL Selection Bit (Selects 4x PLL)
+#pragma config MSSP7B_EN = MSK7 // MSSP address masking (7 Bit address masking mode)
+
+// CONFIG4L
+#pragma config WPFP = PAGE_127  // Write/Erase Protect Page Start/End Location (Write Protect Program Flash Page 127)
+#pragma config WPCFG = OFF      // Write/Erase Protect Configuration Region  (Configuration Words page not erase/write-protected)
+
+// CONFIG4H
+#pragma config WPDIS = OFF      // Write Protect Disable bit (WPFP<6:0>/WPEND region ignored)
+#pragma config WPEND = PAGE_WPFP// Write/Erase Protect Region Select bit (valid when WPDIS = 0) (Pages WPFP<6:0> through Configuration Words erase/write protected)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+
+#if defined(__XC)
+    #include <xc.h>         /* XC8 General Include File */
+#elif defined(HI_TECH_C)
+    #include <htc.h>        /* HiTech General Include File */
+#elif defined(__18CXX)
+    #include <p18cxxx.h>    /* C18 General Include File */
+#endif
+
+/******************************************************************************/
+/* Configuration Bits                                                         */
+/*                                                                            */
+/* Refer to 'HI-TECH PICC and PICC18 Toolchains > PICC18 Configuration        */
+/* Settings' under Help > Contents in MPLAB X IDE for available PIC18         */
+/* Configuration Bit Settings for the correct macros when using the C18       */
+/* compiler.  When using the Hi-Tech PICC18 compiler, refer to the compiler   */
+/* manual.pdf in the compiler installation doc directory section on           */
+/* 'Configuration Fuses'.  The device header file in the HiTech PICC18        */
+/* compiler installation directory contains the available macros to be        */
+/* embedded.  The XC8 compiler contains documentation on the configuration    */
+/* bit macros within the compiler installation /docs folder in a file called  */
+/* pic18_chipinfo.html.                                                       */
+/*                                                                            */
+/* For additional information about what the hardware configurations mean in  */
+/* terms of device operation, refer to the device datasheet.                  */
+/*                                                                            */
+/* General C18/XC8 syntax for configuration macros:                           */
+/* #pragma config <Macro Name>=<Setting>, <Macro Name>=<Setting>, ...         */
+/*                                                                            */
+/* General HiTech PICC18 syntax:                                              */
+/* __CONFIG(n,x);                                                             */
+/*                                                                            */
+/* n is the config word number and x represents the anded macros from the     */
+/* device header file in the PICC18 compiler installation include directory.  */
+/*                                                                            */
+/* A feature of MPLAB X is the 'Generate Source Code to Output' utility in    */
+/* the Configuration Bits window.  Under Window > PIC Memory Views >          */
+/* Configuration Bits, a user controllable configuration bits window is       */
+/* available to Generate Configuration Bits source code which the user can    */
+/* paste into this project.                                                   */
+/*                                                                            */
+/******************************************************************************/
+
+/* TODO Fill in your configuration bits here using the config generator.      */
